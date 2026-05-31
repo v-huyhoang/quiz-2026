f5a916a3736d8980f0bd9079acf5b7852a62f167
ntphuy <ntphuy2001@gmail.com>
2026-05-31 22:19:15 +0700

    perf: switch all broadcast events from ShouldBroadcastNow to ShouldBroadcast
    Admin HTTP response no longer blocks waiting for Reverb to push to all
    200 subscribers. Broadcasts now go through Redis queue (~100-500ms delay
    before clients receive event, acceptable for quiz flow).

--- quiz-be/app/Events/GameFinished.php
+++ quiz-be/app/Events/GameFinished.php
@@ -5,11 +5,11 @@ namespace App\Events;
 use App\Models\Game;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class GameFinished implements ShouldBroadcastNow
+class GameFinished implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
